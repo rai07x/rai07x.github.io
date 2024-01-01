6d11ea20869a3bf534f948027f774903b08d596d
rai07x <rai.jp.kobuki@gmail.com>
2024-01-01 18:26:24 +0900

    m

--- rust-wasm-img-proc/test-wasm_bg.wasm.d.ts
+++ rust-wasm-img-proc/test-wasm_bg.wasm.d.ts
@@ -4,6 +4,6 @@ export const memory: WebAssembly.Memory;
 export function set_value(a: number): void;
 export function get_value(): number;
 export function get_string(a: number): number;
-export function hello_world(a: number): void;
+export function hello_world(): number;
 export function main(a: number, b: number): number;
 export function __wbindgen_start(): void;
